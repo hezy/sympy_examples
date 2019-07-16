951110c25664e29d6010b642b5d5f9e2cfd5705d
hezy <hezy.amiel@gmail.com>
2019-07-17 02:47:41 +0300

    bug fixes for error_propagation
    fixed the squaring of the derivatives
    fixed the numerical calculaion of the error

--- error_propagation.py
+++ error_propagation.py
@@ -12,17 +12,17 @@
 # In[1]:
 
 
-from sympy import init_printing, Function, symbols, Symbol, Derivative, sqrt, log, Eq
-from IPython.display import display
-init_printing(use_latex=True, latex_mode='equation*', forecolor='White') #remove "White" if your background is white 
+from sympy import init_printing, Function, symbols, Symbol, Derivative, sqrt, log, Eq, latex
+from IPython.display import display, Latex
+init_printing(use_latex=True, latex_mode='equation*') 
 
 
 # Define variables
 
-# In[9]:
+# In[2]:
 
 
-F = Function('F')
+#F = Function('F')
 
 f, x, b_s, b_0, b_x = symbols ('f, x, \\beta_s, \\beta_0, \\beta_x')
 
@@ -30,15 +30,14 @@ Df = Symbol('\Delta f')
 Dx = Symbol('\Delta x')
 Db = Symbol('\Delta \\beta')
 
+variables = (x, b_s, b_0, b_x)
+deltadict =	{x: Dx, b_s: Db, b_0: Db, b_x: Db}
+
 
 # Define a mathematical function $F(x, \beta_s, \beta_0, \beta_t)$ :
 
-# In[10]:
+# In[3]:
 
-
-variables = (x, b_s, b_0, b_x)
-
-deltadict =	{x: Dx, b_s: Db, b_0: Db, b_x: Db}
 
 F = 1/x * log( (b_0 - b_s) / (b_x - b_s) )
 
@@ -47,7 +46,7 @@ display(Eq(f,F))
 
 # Calculate the partial derevitives of the function G for each of its variables, sum up the squares of the obtained derivatives:
 
-# In[11]:
+# In[4]:
 
 
 f_sum_squares = 0
@@ -55,13 +54,13 @@ F_sum_squares = 0
 
 for variable in variables:
     display(Eq(Derivative(f, variable), Derivative(F, variable).doit()))
-    f_sum_squares = f_sum_squares + (Derivative(f, variable)) * (deltadict[variable])**2
-    F_sum_squares = F_sum_squares + (Derivative(F, variable)) * (deltadict[variable])**2
+    f_sum_squares = f_sum_squares + (Derivative(f, variable))**2 * (deltadict[variable])**2
+    F_sum_squares = F_sum_squares + (Derivative(F, variable))**2 * (deltadict[variable])**2
 
 
 # The square root of the sum is the estimated error of the function F
 
-# In[12]:
+# In[5]:
 
 
 DF = sqrt (F_sum_squares)
@@ -71,12 +70,13 @@ display(Eq(Df,Df_))
 display(Eq(Df,DF.doit()))
 
 
-# In[14]:
+# In[6]:
 
 
 F_num = F.evalf(subs={x:120, b_0:100, b_s:20, b_x:60})
-display(F_num)
 
-DF_num = DF.evalf(subs={x:120, Dx:1, b_0:100, b_s:20, b_x:60, Db:0.1})
-display(DF_num)
+DF_num = DF.doit().evalf(subs={x:120, Dx:1, b_0:100, b_s:20, b_x:60, Db:0.1})
+
+result = "$$f = {} \pm {}$$".format(latex(F_num), latex(DF_num))
+display(Latex(result))
 
